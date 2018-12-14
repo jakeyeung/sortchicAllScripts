252096ba2aa506cfb5b32d0167790f2106e85024
j.yeung@hubrecht.eu <jyeung@hpcs03.op.umcutrecht.nl>
2018-12-15 00:28:21 +0100

    Summarize read counts across filtered sorted bams

---
+++ 3b-remove_bad_bam_dir.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Jake Yeung
+# 3b-remove_bad_bam_dir.sh
+# There is a dir with empty bam file delete it  
+# 2018-12-15
+
+# dangerous do it manually 
+# rm -r/hpc/hub_oudenaarden/jyeung/data/histone-mods/PZ-BM-m1-H3K9me3-2_AH3VGVBGX9_S1
