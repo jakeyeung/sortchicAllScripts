e45666a8a79b01aaeb470ac81000147361fab4ed
Jake Yeung <j.yeung@hubrecht.eu>
2019-01-04 15:55:27 +0100

    For biorep keep the m suffix. For technical replicate, take the first number rather than S[1-9]

--- scripts/Rfunctions/GetMetaData.R
+++ scripts/Rfunctions/GetMetaData.R
@@ -14,14 +14,16 @@ GetChip <- function(inf){
   return(x)
 }
 GetBioRep <- function(inf){
-  # "PZ-BM-m1-H3K27me3-1_H2GV2BGX9_S14.bc_counts.txt" -> m1 -> 1
+  # "PZ-BM-m1-H3K27me3-1_H2GV2BGX9_S14.bc_counts.txt" -> m1
   x <- strsplit(inf, split = "-")[[1]][[3]]
-  x <- strsplit(x, split = "m")[[1]][[2]]
+  # x <- strsplit(x, split = "m")[[1]][[2]]
   return(x)
 }
 GetTechRep <- function(inf){
-  # "PZ-BM-m1-H3K27me3-1_H2GV2BGX9_S14.bc_counts.txt" -> S14-> 14
-  x <- stringr::str_match(inf, "_S[1-9]*.")[1, 1]
+  # # "PZ-BM-m1-H3K27me3-1_H2GV2BGX9_S14.bc_counts.txt" -> S14-> 14
+  # x <- stringr::str_match(inf, "_S[1-9]*.")[1, 1]
+  # "PZ-BM-m2-H3K9me3-1_H2GV2BGX9_S17" -> 1
+  x <- stringr::str_match(inf, "-[1-9]*_")[1, 1] 
   # remove first and last characters
   x <- substr(x, start = 2, stop = nchar(x) - 1)
   return(x)
