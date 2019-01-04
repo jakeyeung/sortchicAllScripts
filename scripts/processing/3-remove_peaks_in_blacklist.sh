11f6d5859ad0ad8ffc696231ec7cb7df35f4514a
Jake Yeung <j.yeung@hubrecht.eu>
2019-01-04 10:07:03 +0100

    Remove blacklist regions using H3K27me3

--- scripts/processing/3-remove_peaks_in_blacklist.sh
+++ scripts/processing/3-remove_peaks_in_blacklist.sh
@@ -6,9 +6,10 @@
 
 # 
 
-inbed="/hpc/hub_oudenaarden/jyeung/data/scChiC/raw_demultiplexed/merged_bam_macs2_output/BM_H3K4me1_merged.0.3.1000.cutoff/BM_H3K4me1_merged.0.3.1000.cutoff_peaks.broadPeak"
+jchip="H3K27me3"
+inbed="/hpc/hub_oudenaarden/jyeung/data/scChiC/raw_demultiplexed/merged_bam_macs2_output/BM_${jchip}_merged.0.3.1000.cutoff/BM_${jchip}_merged.0.3.1000.cutoff_peaks.broadPeak"
 blacklist="/hpc/hub_oudenaarden/jyeung/data/scChiC/blacklist/mm10.blacklist.bed.gz"
-outbed="/hpc/hub_oudenaarden/jyeung/data/scChiC/raw_demultiplexed/merged_bam_macs2_output/BM_H3K4me1_merged.0.3.1000.cutoff/BM_H3K4me1_merged.0.3.1000.cutoff_peaks.blacklistfilt.broadPeak"
+outbed="/hpc/hub_oudenaarden/jyeung/data/scChiC/raw_demultiplexed/merged_bam_macs2_output/BM_${jchip}_merged.0.3.1000.cutoff/BM_${jchip}_merged.0.3.1000.cutoff_peaks.blacklistfilt.broadPeak"
 
 [[ ! -e $inbed ]] && echo "$inbed not found, exiting" && exit 1
 [[ ! -e $blacklist ]] && echo "$blacklist not found, exiting" && exit 1
