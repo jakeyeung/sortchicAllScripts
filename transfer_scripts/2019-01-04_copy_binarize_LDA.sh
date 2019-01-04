dbe9b21f050fc1ab91f86835f1d95b2de3c1fa89
Jake Yeung <j.yeung@hubrecht.eu>
2019-01-04 10:08:20 +0100

    Init commit for transfers of files

---
+++ transfer_scripts/2019-01-04_copy_binarize_LDA.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Jake Yeung
+# 2019-01-04_copy_binarize_LDA.sh
+# This is run on a local computer
+# 2019-01-04
+
+indir="t2:/hpc/hub_oudenaarden/jyeung/data/scChiC/raw_demultiplexed/lda_analysis.h3k27me3.broadpeaks/lda_outputs.meanfilt_1.merge_1000_NoM_binarize.cellmin_1000.cellmax_50000"
+
+outdir=""
