6a628081fb9c8963eb11eee619bd7a9765208ad5
Jake Yeung <jakeyeung@gmail.com>
2019-01-15 11:13:31 +0100

    Motif analysis

---
+++ scripts/scripts_analysis/lda_model/summarize_histone_marks_downstream.R
@@ -0,0 +1,6 @@
+# Jake Yeung
+# Date of Creation: 2019-01-13
+# File: ~/projects/scChiC/scripts/scripts_analysis/lda_model/summarize_histone_marks_downstream.R
+# After summarizing histone marks, check for weird outliers in the graphs
+
+
