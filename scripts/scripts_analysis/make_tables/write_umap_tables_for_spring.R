115dab5d663fb783a309a90f9ef0b01b3aef69c5
Jake Yeung <jakeyeung@gmail.com>
2019-04-04 12:02:46 +0200

    Do variability analysis acrosss pseudotime

---
+++ scripts/scripts_analysis/make_tables/write_umap_tables_for_spring.R
@@ -0,0 +1,50 @@
+# Jake Yeung
+# Date of Creation: 2019-04-01
+# File: ~/projects/scchic/scripts/scripts_analysis/make_tables/write_umap_tables_for_spring.R
+# Write tables for spring exploration
+
+
+rm(list=ls())
+
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+
+inf <- "/Users/yeung/data/scchic/robjs/TFactivity_genelevels_objects_build95.allmarks_reorient.RData"
+
+load(inf, v=T)
+
+
+# Write topic models list  ------------------------------------------------
+
+# do only one mark
+jmark <- "H3K4me1"
+
+cell.topic.mat <- tm.result.lst[[jmark]]$topics
+
+imputed.mat <- as.data.frame(tm.result.lst[[jmark]]$topics %*% tm.result.lst[[jmark]]$terms)
+
+cell.bcs <- rownames(imputed.mat)
+
+imputed.mat.df <- data.frame(barcodes = cell.bcs, imputed.mat)
+
+print(dim(imputed.mat.df))
+
+# write imputed.mat to output 
+outf <- paste0("/Users/yeung/data/scchic/tables/imputed_matrix/", jmark, "_imputed_matrix_transpose.txt")
+outf.genelst <- paste0("/Users/yeung/data/scchic/tables/imputed_matrix/", jmark, "_genelist.txt")
+
+data.table::fwrite(imputed.mat.df, file = outf)
+system(paste0("gzip ", outf))
+
+# create gene list
+gene.lst <- colnames(imputed.mat)
+write(gene.lst, file = outf.genelst)
+
+
+# write.table(imputed.mat, 
+#           file = outf, 
+#           sep = "\t",
+#           quote = FALSE)
+# system(paste0("gzip ", outf))
+
