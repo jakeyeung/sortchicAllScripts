f401cc13feffa7bbb2aa589c3ee4673f8eb3104d
Jake Yeung <j.yeung@hubrecht.eu>
2019-04-11 09:30:39 +0200

    Init files for merging bams

---
+++ scripts/processing/merge_bams_by_cluster/merge_bams_build95_2019-03-26/4e-bedgraph_to_bigwig.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# Jake Yeung
+# 4d-bedgraph_to_bigwig.sh
+# bedGraphToBigWig 
+# 2019-03-21
+
+chromsizes="/hpc/hub_oudenaarden/jyeung/data/databases/chromsizes/chromsizes.mm10.txt"
+inmain="/hpc/hub_oudenaarden/jyeung/data/scChiC/raw_demultiplexed/bam_cluster_merged/bedgraphs_build95_2019-03-28/log1p/chr_filt"
+outmain="/hpc/hub_oudenaarden/jyeung/data/scChiC/raw_demultiplexed/bam_cluster_merged/bigwigs_build95_2019-03-28//log1p_bigwigs"
+
+[[ ! -d $outmain ]] && mkdir $outmain
+
+[[ ! -e $chromsizes ]] && echo "$chromsizes not found, exiting" && exit 1
+[[ ! -d $inmain ]] && echo "$inmain not found, exiting" && exit 1
+
+n=0
+maxjobs=16
+for inbed in `ls -d $inmain/*.bedgraph`; do
+    # echo $inbed
+    bbase=$(basename $inbed)
+    bbase=${bbase%.*}
+    outbed=$outmain/$bbase.log1p.bw
+    bedGraphToBigWig $inbed $chromsizes $outbed&
+if (( $(($((++n)) % $maxjobs)) == 0 )) ; then
+	# define maxjobs and n using maxjobsn skeleton
+    wait # wait until all have finished (not optimal, but most times good enough)
+    echo $n wait
+fi
+done
+wait
