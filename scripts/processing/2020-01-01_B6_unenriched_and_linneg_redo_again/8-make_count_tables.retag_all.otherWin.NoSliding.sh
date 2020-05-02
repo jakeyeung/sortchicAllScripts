6011ec1817dee84e9cbf5cab41e22112247eb942
Jake Yeung <j.yeung@hubrecht.eu>
2020-05-02 16:45:03 +0200

    Old commit analysis from beginning of year

---
+++ scripts/processing/2020-01-01_B6_unenriched_and_linneg_redo_again/8-make_count_tables.retag_all.otherWin.NoSliding.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+# Jake Yeung
+# 7-make_count_tables.sh
+#  
+# 2019-09-04
+
+
+inmain="/hpc/hub_oudenaarden/jyeung/data/scChiC/raw_data/ZellerRawDataB6_redo_2019-12-13.tagged_bams_mergedbymarks"
+outdir=$inmain/countTables_otherWinSize_NoSliding
+[[ ! -d $outdir ]] && mkdir $outdir
+
+jmem='64G'
+jtime='4:00:00'
+
+mapq=40
+# stepsize=20000
+# binsize=10000
+# bsizes="5000 10000 20000 50000"
+bsizes="20000 50000"
+
+for bsize in $bsizes; do
+    # stepsize=$(expr $bsize / 2)
+    stepsize=$bsize
+    # stepsize=$(echo $((x / y)))
+    # inbam="/hpc/hub_oudenaarden/jyeung/data/scChiC/raw_data/ZellerRawDataB6_mergedAll.retag/H3K4me3-BM_SC-merged.tagged.retagged.bam"
+    for inbam in `ls -d $inmain/*.bam`; do
+        [[ ! -e $inbam ]] && echo "$inbam not found, exiting" && exit 1
+        bname=$(basename $inbam)
+        bname=${bname%.*}
+        outf=$outdir/${bname}.bsize_${bsize}.step_${stepsize}.countTable.demuxbugfixed.csv
+        [[ -e $outf ]] && echo "$outf found, continuing" && continue
+
+        BNAME=$outdir/${bname}.counttables.qsub
+        DBASE=$(dirname "${BNAME}")
+        [[ ! -d $DBASE ]] && echo "$DBASE not found, exiting" && exit 1
+
+        echo ". /hpc/hub_oudenaarden/jyeung/software/anaconda3/etc/profile.d/conda.sh; conda activate py3; bamToCountTable.py -sliding $stepsize --filterXA -minMQ $mapq $inbam -o $outf -sampleTags SM -joinedFeatureTags reference_name -bin $bsize -binTag DS --dedup" | qsub -l h_rt=${jtime} -l h_vmem=${jmem} -o ${BNAME}.out -e ${BNAME}.err -pe threaded 1 -N $bname.$bsize.countTable
+        # . /hpc/hub_oudenaarden/jyeung/software/anaconda3/etc/profile.d/conda.sh; conda activate py3; bamToCountTable.py -sliding $stepsize --filterXA -minMQ $mapq $inbam -o $outf -sampleTags SM -joinedFeatureTags reference_name -bin $bsize -binTag DS --dedup
+        # exit 0
+
+    done
+done
+wait
+
---
+++ scripts/processing/2020-01-01_B6_unenriched_and_linneg_redo_again/9-filter_counts_blacklist.OtherWinSize.NoSliding.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+# Jake Yeung
+# 6b-filter_counts_blacklist.sh
+#  
+# 2019-12-25
+
+# WRAP UP
+while [[ `qstat | grep H3K |  wc -l` > 0 ]]; do
+        echo "sleep for 60 seconds"
+        sleep 60
+done
+
+jmem='48G'
+jtime='4:00:00'
+
+rs="/home/hub_oudenaarden/jyeung/projects/scchic-functions/scripts/processing_scripts/filter_good_cells_good_bins.R"
+[[ ! -e $rs ]] && echo "$rs not found, exiting" && exit 1
+
+dirlh="/hpc/hub_oudenaarden/jyeung/data/scChiC/raw_data/ZellerRawDataB6_redo_2019-12-13.tagged_bams_mergedbymarks/LHcounts"
+dircounts="/hpc/hub_oudenaarden/jyeung/data/scChiC/raw_data/ZellerRawDataB6_redo_2019-12-13.tagged_bams_mergedbymarks/countTables_otherWinSize_NoSliding"
+
+outmain=${dircounts}/qc_filt
+[[ ! -d $outmain ]] && mkdir $outmain
+blfile="/hpc/hub_oudenaarden/jyeung/data/databases/blacklists/mm10.blacklist.copy.bed.gz"
+
+binsizes="20000 50000"
+
+for binsize in $binsizes; do
+    # stepsize=$(expr $binsize / 2)
+    stepsize=$binsize
+
+    infcounts1=${dircounts}/H3K4me1-BM_SC-merged.tagged.bsize_${binsize}.step_${stepsize}.countTable.demuxbugfixed.csv
+    infcounts2=${dircounts}/H3K4me3-BM_Linneg_SC-merged.tagged.bsize_${binsize}.step_${stepsize}.countTable.demuxbugfixed.csv
+    infcounts3=${dircounts}/H3K27me3-BM_Linneg_SC-merged.tagged.bsize_${binsize}.step_${stepsize}.countTable.demuxbugfixed.csv
+    infcounts4=${dircounts}/H3K9me3-BM_Linneg_SC-merged.tagged.bsize_${binsize}.step_${stepsize}.countTable.demuxbugfixed.csv
+
+    inf1=${dirlh}/H3K4me1-BM_SC-merged.tagged.LH_counts.csv
+    inf2=${dirlh}/H3K4me3-BM_Linneg_SC-merged.tagged.LH_counts.csv
+    inf3=${dirlh}/H3K27me3-BM_Linneg_SC-merged.tagged.LH_counts.csv
+    inf4=${dirlh}/H3K9me3-BM_Linneg_SC-merged.tagged.LH_counts.csv
+
+    for inf in $inf1 $inf2 $inf3 $inf4 $infcounts1 $infcounts2 $infcounts3 $infcounts4; do
+        [[ ! -e $inf ]] && echo "$inf not found, exiting" && exit 1
+    done
+
+    outdir=${outmain}/qc_${binsize}_${stepsize}
+    [[ ! -d $outdir ]] && mkdir $outdir
+
+    BNAME=${outdir}/qc_${binsize}_${stepsize}.qsub
+    DBASE=$(dirname "${BNAME}")
+    [[ ! -d $DBASE ]] && echo "$DBASE not found, exiting" && exit 1
+
+    echo ". /hpc/hub_oudenaarden/jyeung/software/anaconda3/etc/profile.d/conda.sh; conda activate R3.6; Rscript $rs -infilerz $inf1 $inf2 $inf3 $inf4 $inf5 -infilecounts $infcounts1 $infcounts2 $infcounts3 $infcounts4 $infcounts5 -names H3K4me1 H3K4me3 H3K27me3 H3K9me3 -outdir $outdir -countcutoff 1000 500 1000 1000 -TAcutoff 0.5 -blfile $blfile --overwrite" | qsub -l h_rt=${jtime} -l h_vmem=${jmem} -o ${BNAME}.out -e ${BNAME}.err -pe threaded 1 -N qc_${binsize}${stepsize}
+done
